2f0725b38fa16d20fc05ef4231b790c5f153faa2
Margarita0509 <62116589+Margarita0509@users.noreply.github.com>
2021-03-19 18:49:25 +0300

    Create child.c

---
+++ child.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+int main() 
+{ 
+    printf("Hi from child.c\n");
+    printf("PID of new process is: %d\n", getpid());
+    return 0; 
+}
+© 2021 GitHub, Inc.
